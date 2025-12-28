40a3b024043cef23ad32a2044fce7383b104dc6c
Marcel Wagner <info@marcelwagner.dev>
2025-12-28 22:12:53 +0100

    Add Discord Note

--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -24,11 +24,17 @@ import 'vue-sonner/style.css';
 const props = usePage().props;
 const sessionId = props.session.id;
 const version = ref(props.github_version || '');
-const alert = ref({
-  title: 'Update',
-  description:
-    'Standard Maximalgröße beträgt nun 2000 MB. Auf dem pr0gramm können wir nun Videos bis zu 2 GB hochladen.',
-});
+const alerts = ref([
+  {
+    title: 'Discord Server',
+    description:
+      'Ich habe einen Discord-Server angelegt, auf dem ihr mir Feedback geben könnt und über fehlgeschlagene Konvertierungen berichten könnt.',
+    button: {
+      text: 'Dem Server beitreten',
+      link: 'https://discord.gg/gubnTf4mMK',
+    },
+  },
+]);
 
 onMounted(() => {
   // eslint-disable-next-line no-undef
@@ -132,6 +138,26 @@ const logout = async () => {
               <GithubIcon />
             </NavigationMenuLink>
           </NavigationMenuItem>
+          <NavigationMenuItem class="block w-full">
+            <NavigationMenuLink
+              target="_blank"
+              href="https://discord.gg/gubnTf4mMK"
+              :class="[
+                navigationMenuTriggerStyle(),
+                'w-full! justify-center!',
+              ]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="size-6"
+                viewBox="0 0 16 16">
+                <path
+                  d="M13.545 2.907a13.2 13.2 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.2 12.2 0 0 0-3.658 0 8 8 0 0 0-.412-.833.05.05 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.04.04 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032q.003.022.021.037a13.3 13.3 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019q.463-.63.818-1.329a.05.05 0 0 0-.01-.059l-.018-.011a9 9 0 0 1-1.248-.595.05.05 0 0 1-.02-.066l.015-.019q.127-.095.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.05.05 0 0 1 .053.007q.121.1.248.195a.05.05 0 0 1-.004.085 8 8 0 0 1-1.249.594.05.05 0 0 0-.03.03.05.05 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.2 13.2 0 0 0 4.001-2.02.05.05 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.03.03 0 0 0-.02-.019m-8.198 7.307c-.789 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612m5.316 0c-.788 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612"></path>
+              </svg>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
           <NavigationMenuItem
             v-if="$page.props.user !== null"
             class="block w-full cursor-pointer">
@@ -189,7 +215,25 @@ const logout = async () => {
               href="https://github.com/Tschucki/pr0verter"
               :class="[navigationMenuTriggerStyle(), 'w-full! justify-start!']">
               <GithubIcon />
-              - GitHub
+              &nbsp;&nbsp;GitHub
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem class="block w-full">
+            <NavigationMenuLink
+              target="_blank"
+              href="https://discord.gg/gubnTf4mMK"
+              :class="[navigationMenuTriggerStyle(), 'w-full! justify-start!']">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="size-6"
+                viewBox="0 0 16 16">
+                <path
+                  d="M13.545 2.907a13.2 13.2 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.2 12.2 0 0 0-3.658 0 8 8 0 0 0-.412-.833.05.05 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.04.04 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032q.003.022.021.037a13.3 13.3 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019q.463-.63.818-1.329a.05.05 0 0 0-.01-.059l-.018-.011a9 9 0 0 1-1.248-.595.05.05 0 0 1-.02-.066l.015-.019q.127-.095.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.05.05 0 0 1 .053.007q.121.1.248.195a.05.05 0 0 1-.004.085 8 8 0 0 1-1.249.594.05.05 0 0 0-.03.03.05.05 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.2 13.2 0 0 0 4.001-2.02.05.05 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.03.03 0 0 0-.02-.019m-8.198 7.307c-.789 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612m5.316 0c-.788 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612"></path>
+              </svg>
+              &nbsp;&nbsp;Discord
             </NavigationMenuLink>
           </NavigationMenuItem>
           <NavigationMenuItem
@@ -206,10 +250,23 @@ const logout = async () => {
     </NavigationMenu>
   </header>
   <main class="mx-auto max-w-4xl px-4 py-6">
-    <Alert v-if="alert.title" class="mb-8">
+    <Alert
+      v-for="alert in alerts.filter((a) => a.title && a.description)"
+      class="mb-8"
+      :key="index"
+      :variant="alert.variant || 'info'">
       <AlertTitle>{{ alert.title }}</AlertTitle>
       <AlertDescription>
         {{ alert.description }}
+        <template v-if="alert.button">
+          <br />
+          <br />
+          <a :href="alert.button.link" target="_blank" class="w-full">
+            <Button type="button" class="w-full">
+              {{ alert.button.text }}
+            </Button>
+          </a>
+        </template>
       </AlertDescription>
     </Alert>
     <slot />
