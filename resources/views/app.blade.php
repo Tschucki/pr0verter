8b23e112ebf659d474d05d75a2192fe569b9607d
Marcel Wagner <info@marcelwagner.dev>
2026-04-09 22:02:20 +0200

    Update Plausible analytics script and initialization
    Replaced Plausible analytics script with a new version and added initialization code.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
     <link rel="icon" sizes="32x32 16x16" href="/favicon.ico">
-    <script defer data-domain="pr0verter.de" src="https://plausible.marcelwagner.dev/js/script.js"></script>
+    <!-- Privacy-friendly analytics by Plausible -->
+    <script async src="https://plausible.marcelwagner.dev/js/pa-kCJsKz6aQVDj5gyj8CUE5.js"></script>
+    <script>
+      window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+      plausible.init()
+    </script>
     @routes
     @vite('resources/js/app.js')
     @inertiaHead
